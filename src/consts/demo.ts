821e4b64987bcfa0feac94379d98b725307af05d
Mone-Lee <1051933474@qq.com>
2025-11-28 17:23:42 +0800

    feat: add fe-weapp-ui-pro demo url map

--- src/consts/demo.ts
+++ src/consts/demo.ts
@@ -1,6 +1,7 @@
 
 // TODO: 优化组件库的文档命名
-export const DEMO_PATH_MAP: Record<string, string> = {
+// 基础库组件对应的 demo 文档路径映射
+const DEMO_PATH_MAP: Record<string, string> = {
   FQBadge: 'packages/fq-weapp-ui-doc/docs/components/presentation/Badge 徽标数.mdx',
   FQButton: 'packages/fq-weapp-ui-doc/docs/components/basic/Button 按钮.mdx',
   FQCard: 'packages/fq-weapp-ui-doc/docs/components/presentation/Card 卡片.mdx',
@@ -19,3 +20,18 @@ export const DEMO_PATH_MAP: Record<string, string> = {
   FQTextareaNew: 'packages/fq-weapp-ui-doc/docs/components/form/TextArea 输入框.mdx',
   FQWaterMark: 'packages/fq-weapp-ui-doc/docs/components/reaction/FQWaterMark 水印.mdx',
 }
+
+
+// 业务组件对应的 demo 文档路径映射
+const PRO_DEMO_PATH_MAP: Record<string, string> = {
+  FQGoodsCard: 'packages/fq-weapp-ui-doc/docs/components-pro/GoodsCard 商品卡片.mdx',
+  FQPrice: 'packages/fq-weapp-ui-doc/docs/components-pro/Price 价格.mdx',
+  FQSearch: 'packages/fq-weapp-ui-doc/docs/components-pro/Search 搜索框.mdx',
+  FQVideoPlayer: 'packages/fq-weapp-ui-doc/docs/components-pro/VideoPlayer 视频播放器.mdx',
+}
+
+// 合并基础库和业务组件的 demo 文档路径映射
+export const FULL_DEMO_PATH_MAP: Record<string, string> = {
+  ...DEMO_PATH_MAP,
+  ...PRO_DEMO_PATH_MAP,
+};
